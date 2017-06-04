e3fc4756c54375390e11f43c9c1fe4dcf6d56558
hoolly <hoollyzhang@tencent.com>
2017-06-04 22:21:14 +0800

    【修复】修复语音再次播放会失败的问题。

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -8,7 +8,7 @@ Page({
    */
   data: {
   },
-  audioError:function(e){
+  audioError: function (e) {
     console.log(e)
   },
   /**
@@ -23,66 +23,66 @@ Page({
       flippedword: flippedword,
       animateStatus: 'stopanimate'
     })
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
 
-  imagePreview : function(event){
+  imagePreview: function (event) {
     var url = event.currentTarget.dataset.url
     wx.previewImage({
-      urls : [url]
+      urls: [url]
     })
   },
 
-  play: function (event){
+  play: function (event) {
     this.setData({
       animateStatus: ''
     })
@@ -91,22 +91,28 @@ Page({
 
     var i = 0
     var that = this
-    for (i; i < flippedword.contents.length;i++){
+    for (i; i < flippedword.contents.length; i++) {
       var content = flippedword.contents[i]
-      if (content.type == 'audio'){
+      if (content.type == 'audio') {
         let j = i
-        flippedword.contents[j].text = flippedword.contents[j].text.replace(/http/, "https")
-        wx.downloadFile({
-          url: flippedword.contents[j].text, 
-          success: function (res) {
-            wx.playVoice({
-              filePath: res.tempFilePath
-            })
+        if (flippedword.contents[j].text.startsWith('wxfile:')) {
+          wx.playVoice({
+            filePath: flippedword.contents[j].text
+          })
+        } else {
+          flippedword.contents[j].text = flippedword.contents[j].text.replace(/http/, "https")
+          wx.downloadFile({
+            url: flippedword.contents[j].text,
+            success: function (res) {
+              wx.playVoice({
+                filePath: res.tempFilePath
+              })
 
-            // flippedword.contents[j].text = res.tempFilePath
-            
-          }
-        })
+              flippedword.contents[j].text = res.tempFilePath
+
+            }
+          })
+        }
       }
     }
   }
